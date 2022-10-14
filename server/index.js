57e08800f8575481b0a9096385e7353de9f649a2
Garab Rezső <47292831+GarabRezso@users.noreply.github.com>
2022-10-14 16:52:21 +0200

    updated index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 app.use('/posts', postRoutes);
-const CONNECTION_URL = 'mongodb+srv://garabrezso:gr123qwe@cluster0.vd0m6ih.mongodb.net/?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://<username>:<password>@cluster0.vd0m6ih.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
